c6537d7a0a092d01003cc7923f97af6a1b76bf55
UtopiaMint <admin@lkp111138.me>
2019-10-04 01:49:11 +0800

    Added US and UK translations to /setlang

--- src/main/java/me/lkp111138/dealbot/commands/SetLangCommand.java
+++ src/main/java/me/lkp111138/dealbot/commands/SetLangCommand.java
@@ -49,7 +49,9 @@ public class SetLangCommand implements Command {
                 bot.execute(new SendMessage(gid, "語言設定 / Language Settings").replyMarkup(new InlineKeyboardMarkup(
                         new InlineKeyboardButton[]{new InlineKeyboardButton("繁體中文").callbackData("lang:zh:" + uid)},
                         new InlineKeyboardButton[]{new InlineKeyboardButton("廣東話").callbackData("lang:hk:" + uid)},
-                        new InlineKeyboardButton[]{new InlineKeyboardButton("English").callbackData("lang:en:" + uid)},
+                        new InlineKeyboardButton[]{new InlineKeyboardButton("English (HK)").callbackData("lang:en:" + uid)},
+                        new InlineKeyboardButton[]{new InlineKeyboardButton("English (US)").callbackData("lang:en_us:" + uid)},
+                        new InlineKeyboardButton[]{new InlineKeyboardButton("English (UK)").callbackData("lang:en_gb:" + uid)},
                         new InlineKeyboardButton[]{new InlineKeyboardButton(Translation.get(DealBot.lang(gid)).CLOSE()).callbackData("lang:close:" + uid)}
                 )));
         }
