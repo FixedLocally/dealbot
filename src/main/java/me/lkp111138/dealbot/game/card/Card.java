bc7296b74482e07187774f90f08bab0a178c7a42
UtopiaMint <admin@lkp111138.me>
2019-12-29 16:59:24 +0800

    Added execute() and getCurrencyValue() to Cards

--- src/main/java/me/lkp111138/dealbot/game/card/Card.java
+++ src/main/java/me/lkp111138/dealbot/game/card/Card.java
@@ -1,5 +1,7 @@
 package me.lkp111138.dealbot.game.card;
 
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import me.lkp111138.dealbot.game.Player;
 import me.lkp111138.dealbot.game.card.state.CardState;
 
 public interface Card {
@@ -17,4 +19,17 @@ public interface Card {
      * @return The card's current state
      */
     CardState getState();
+
+    /**
+     * @return The card's currency value
+     */
+    int getCurrencyValue();
+
+    /**
+     * Defines the actions when the card is used
+     * @param player The Player that attempted to use this card
+     * @param arg The optional argument to be passed to the card
+     * @return An inline keyboard if the card needs further arguments or null
+     */
+    InlineKeyboardMarkup execute(Player player, String arg);
 }
