e1f840873356f393ab90b8d2dc64497c8cc7d09e
UtopiaMint <admin@lkp111138.me>
2019-12-29 19:22:12 +0800

    Added property set counts and rents

--- src/main/java/me/lkp111138/dealbot/game/card/PropertyCard.java
+++ src/main/java/me/lkp111138/dealbot/game/card/PropertyCard.java
@@ -1,4 +1,17 @@
 package me.lkp111138.dealbot.game.card;
 
 public interface PropertyCard extends Card {
+    int[] propertySetCounts = {2, 3, 3, 3, 3, 3, 3, 2, 4, 2};
+    int[][] propertyRents = {
+            {1, 2},
+            {1, 2, 3},
+            {1, 2, 4},
+            {1, 3, 5},
+            {2, 3, 6},
+            {2, 4, 6},
+            {2, 4, 7},
+            {3, 8},
+            {1, 2, 3, 4},
+            {1, 2}
+    };
 }
