9239f6e21a12da720285566ca935b146f8578ffe
UtopiaMint <admin@lkp111138.me>
2020-01-03 14:24:10 +0800

    Made CardArgumentRequest capable of returning an objection-able action

--- src/main/java/me/lkp111138/dealbot/game/CardArgumentRequest.java
+++ src/main/java/me/lkp111138/dealbot/game/CardArgumentRequest.java
@@ -5,10 +5,24 @@ import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 public class CardArgumentRequest {
     private InlineKeyboardButton[][] keyboard;
     private String message;
+    private Runnable objectionable;
+    private Type type;
 
+    /**
+     * Constructs an object that represents updating the current message
+     */
     public CardArgumentRequest(InlineKeyboardButton[][] keyboard, String message) {
         this.keyboard = keyboard;
         this.message = message;
+        this.type = Type.UPDATE;
+    }
+
+    /**
+     * Constructs an object that pauses the turn and starts the objection process
+     */
+    public CardArgumentRequest(Runnable objectionable) {
+        this.objectionable = objectionable;
+        this.type = Type.OBJECTION;
     }
 
     public InlineKeyboardButton[][] getKeyboard() {
@@ -17,5 +31,18 @@ public class CardArgumentRequest {
 
     public String getMessage() {
         return message;
+    }
+
+    public Runnable getObjectionable() {
+        return objectionable;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public enum Type {
+        UPDATE,
+        OBJECTION
     }
 }
