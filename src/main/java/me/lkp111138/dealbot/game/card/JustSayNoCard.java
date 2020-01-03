84cd943d0fe67dfdb7abd95e42823fc1b87041a0
UtopiaMint <admin@lkp111138.me>
2020-01-03 17:52:52 +0800

    Objection card actions and strings

--- src/main/java/me/lkp111138/dealbot/game/card/JustSayNoCard.java
+++ src/main/java/me/lkp111138/dealbot/game/card/JustSayNoCard.java
@@ -1,10 +1,12 @@
 package me.lkp111138.dealbot.game.card;
 
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import me.lkp111138.dealbot.DealBot;
 import me.lkp111138.dealbot.game.CardArgumentRequest;
 import me.lkp111138.dealbot.game.Player;
 import me.lkp111138.dealbot.game.card.state.CardState;
 import me.lkp111138.dealbot.game.card.state.CardStateInMainDeck;
+import me.lkp111138.dealbot.game.card.state.CardStateInPlayerCurrency;
 
 public class JustSayNoCard implements Card {
     private int id;
@@ -46,6 +48,17 @@ public class JustSayNoCard implements Card {
 
     @Override
     public CardArgumentRequest execute(DealBot bot, Player player, String[] arg) {
-        return null;
+        if (arg.length == 0) {
+            String message = String.format("%s\n%s%s",
+                    bot.translate(player.getUserId(), "game.objection_card_prompt", bot.translate(player.getUserId(), getNameKey())),
+                    bot.translate(player.getUserId(), "game.card_desc"), bot.translate(player.getUserId(), getDescriptionKey()));
+            InlineKeyboardButton[][] buttons = new InlineKeyboardButton[2][1];
+            buttons[1][0] = new InlineKeyboardButton(bot.translate(player.getUserId(), "card.use_as_currency"))
+                    .callbackData("arg:money");
+            return new CardArgumentRequest(buttons, message);
+        } else {
+            setState(new CardStateInPlayerCurrency(player));
+            return null;
+        }
     }
 }
